9ec4b3ef5b99b2ba242440f303ed4739147067d1
HayrapetyanStyle <hayrapetyanstyle@gmail.com>
2021-01-04 00:08:55 +0400

    Timer module for the website

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import timer from './modules/timer';
+
+window.addEventListener('DOMContentLoaded', function() {
+    timer('.timer', '2021-03-21');
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/js'
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {}
+};
